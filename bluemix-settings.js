9d5ca096088708fb3f470c195199318b8c0f6613
unknown <anders.langseth@MW7XP5FOKEGVR0.dir.svc.accenture.com>
2016-05-02 13:41:10 +0200

    he

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -60,7 +60,8 @@ var settings = module.exports = {
         googleapis:googleapis,
         GoogleTokenProvider:GoogleTokenProvider,
         CLIENT_ID : CLIENT_ID_WEB_APP,
-        CLIENT_SECRET : CLIENT_SECRET_WEB_APP
+        CLIENT_SECRET : CLIENT_SECRET_WEB_APP, 
+        VCAP_APPLICATION: JSON.parse(process.env.VCAP_APPLICATION)
     },
 
     storageModule: require("./couchstorage")
